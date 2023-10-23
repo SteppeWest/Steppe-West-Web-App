44eeab7dc718cc5884a04b831e9b20068a1f4504
p2made <pedrofp@me.com>
2023-10-23 20:43:34 +1000

    zing

---
+++ unpub/scratch/php_scratch_00.php
@@ -0,0 +1,27 @@
+					<hr>
+					<!-- / WhatsApp
+						/ -->
+					<a href="https://wa.me/61400473376?text=About%20Steppe%20West" class="btn btn-warning btn-circle btn-circle-lg m-1" role="button" target="_blank" aria-disabled="true">
+						<img height="32" width="32" style="vertical-align:middle;margin: 0px 0px 6px 0px" src="https://cdn.simpleicons.org/whatsapp/222" alt="WhatsApp">
+					</a>
+					<!-- / Telegram
+						/ -->
+					<a href="https://t.me/steppewest" class="btn btn-warning btn-circle btn-circle-lg m-1" role="button" target="_blank" aria-disabled="true">
+						<img height="32" width="32" style="vertical-align:middle;margin: 0px 0px 6px 0px" src="https://cdn.simpleicons.org/telegram/222" alt="Telegram">
+					</a>
+					<!-- / Viber
+						/ -->
+					<a href="viber://add?number=61400473376" class="btn btn-warning btn-circle btn-circle-lg m-1" role="button" target="_blank" aria-disabled="true">
+						<img height="32" width="32" style="vertical-align:middle;margin: 0px 0px 6px 0px" src="https://cdn.simpleicons.org/viber/222" alt="Viber">
+					</a>
+
+
+					<a href="tel:%2B61400473376" class="btn btn-outline-warning btn-circle m-1" role="button" aria-disabled="true">
+						<i class="fas fa-phone fa-lg"></i>
+					</a>
+					<a href="sms:%2B61400473376" class="btn btn-outline-warning btn-circle m-1" role="button" aria-disabled="true">
+						<i class="fas fa-comment-dots fa-lg"></i>
+					</a>
+					<a href="mailto:pedro@steppewest.com?subject=Steppe West" class="btn btn-outline-warning btn-circle m-1" role="button" aria-disabled="true">
+						<i class="far fa-envelope fa-lg"></i>
+					</a>
