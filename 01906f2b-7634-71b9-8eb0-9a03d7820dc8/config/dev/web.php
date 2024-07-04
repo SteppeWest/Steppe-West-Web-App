6ff3a6d4b80f74ea9a485df6185db7fd9450d616
p2made <pedrofp@me.com>
2024-07-05 04:48:11 +1000

    restore base config

---
+++ 01906f2b-7634-71b9-8eb0-9a03d7820dc8/config/dev/web.php
@@ -0,0 +1,33 @@
+<?php
+use yii\helpers\ArrayHelper;
+
+$baseConfig = require __DIR__ . '/../web.php';
+
+$config = [
+	'components' => [
+		'request' => [
+			// !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+			'cookieValidationKey' => 'LFZMHWb2bcuOxDbi13fUWBOedA6ZJeyk',
+		],
+	],
+];
+
+// only in dev
+if (YII_ENV_DEV) {
+	// configuration adjustments for 'dev' environment
+	$config['bootstrap'][] = 'debug';
+	$config['modules']['debug'] = [
+		'class' => 'yii\debug\Module',
+		// uncomment the following to add your IP if you are not connecting from localhost.
+		//'allowedIPs' => ['127.0.0.1', '::1'],
+	];
+
+	$config['bootstrap'][] = 'gii';
+	$config['modules']['gii'] = [
+		'class' => 'yii\gii\Module',
+		// uncomment the following to add your IP if you are not connecting from localhost.
+		//'allowedIPs' => ['127.0.0.1', '::1'],
+	];
+}
+
+return ArrayHelper::merge($baseConfig, $config);
---
+++ 01906f2b-7634-71b9-8eb0-9a03d7820dc8/config/invite/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\helpers\ArrayHelper;
+
+$baseConfig = require __DIR__ . '/../web.php';
+
+$config = [
+	'id' => 'invite',
+	'components' => [
+		// subdomain-specific components configuration
+		'request' => [
+			// !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+			'cookieValidationKey' => 'K0YblPxGT2vLsM896ex5CVYLsH0GNws2',
+		],
+		'view' => [
+			'theme' => [
+				'pathMap' => [
+					'@app/views' => [
+						'@app/themes/base',
+						//'@app/themes/admin',
+						//'@app/themes/letter',
+						//'@app/themes/links',
+					]
+					//'@app/modules' => '@app/themes/basic/modules',
+					//@app/widgets' => '@app/themes/basic/widgets',
+				],
+			],
+		],
+	],
+	// other subdomain-specific configurations
+];
+
+if (YII_ENV_DEV) {
+	// configuration adjustments for 'dev' environment
+	$config['bootstrap'][] = 'debug';
+	$config['modules']['debug'] = [
+		'class' => 'yii\debug\Module',
+		// uncomment the following to add your IP if you are not connecting from localhost.
+		//'allowedIPs' => ['127.0.0.1', '::1'],
+	];
+
+	$config['bootstrap'][] = 'gii';
+	$config['modules']['gii'] = [
+		'class' => 'yii\gii\Module',
+		// uncomment the following to add your IP if you are not connecting from localhost.
+		//'allowedIPs' => ['127.0.0.1', '::1'],
+	];
+}
+
+return ArrayHelper::merge($baseConfig, $config);
