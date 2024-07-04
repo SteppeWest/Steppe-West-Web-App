874fcb861d2bcbf1b486298f32ada2f52332aee2
p2made <pedrofp@me.com>
2024-07-05 06:22:30 +1000

    add controllers

---
+++ 01906f2b-7634-71b9-8eb0-9a03d7820dc8/languages/_code/create.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+
+/** @var yii\web\View $this */
+/** @var app\models\SWLanguageCode $model */
+
+$this->title = Yii::t('app', 'Create Sw Language Code');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Sw Language Codes'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="swlanguage-code-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
