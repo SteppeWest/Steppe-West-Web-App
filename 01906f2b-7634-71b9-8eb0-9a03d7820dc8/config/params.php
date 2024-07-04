75cdd499d33bd1c681717fbafeb186f3fec0a121
p2made <pedrofp@me.com>
2024-07-05 01:36:03 +1000

    reset

--- 01906f2b-7634-71b9-8eb0-9a03d7820dc8/config/params.php
+++ 01906f2b-7634-71b9-8eb0-9a03d7820dc8/config/params.php
@@ -1,30 +1,7 @@
 <?php
 
 return [
-	'adminEmail' => 'admin@example.com',
-	'senderEmail' => 'noreply@example.com',
-	'senderName' => 'Example.com mailer',
-	'swDefaultLanguage' => 'en',
-	'swLanguages' => [
-		'az',
-		'en',
-		'kk',
-		'ky',
-		'mn',
-		'ru',
-		'tg',
-		'tk',
-		'uz',
-	],
-	'swLanguageMap' => [
-		'EN' => 'en',
-		'RU' => 'ru',
-		'KZ' => 'kk',
-		'KG' => 'ky',
-		'TJ' => 'tg',
-		'TM' => 'tk',
-		'UZ' => 'uz',
-		'AZ' => 'az',
-		'MN' => 'mn',
-	],
+    'adminEmail' => 'admin@example.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
 ];
