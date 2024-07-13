d99d3d68bd932f17203d4f419bdeccc71098c67f
p2made <pedrofp@me.com>
2024-07-14 00:33:44 +1000

    shift sw_invite

--- sw_invite/index.php
+++ sw_invite/index.php
@@ -1,1 +1,20 @@
-<?php $updated='2024-07-10';$canonical='https://invite.steppewest.com/';include 'inc/app.php';init(); ?><!doctypehtml><html lang="<?=$languageData['lang']?>"><?php include 'inc/partials/head.min.php'; ?><body><?php include 'inc/partials/header.min.php'; ?><section class="py-1"><?php include 'inc/partials/languages.min.php';include 'inc/partials/content.min.php';include 'inc/partials/signature.min.php'; ?></section><?php include 'inc/partials/footer.min.php'; ?></body></html>
+<?php
+$endpoint = 'letter';
+$appfolder  = '0190888a-c7e8-7011-8121-33bd8b8c4763';
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require __DIR__ . '/../' . $appfolder . '/vendor/autoload.php';
+require __DIR__ . '/../' . $appfolder . '/vendor/yiisoft/yii2/Yii.php';
+require __DIR__ . '/../' . $appfolder . '/common/config/bootstrap.php';
+require __DIR__ . '/../' . $appfolder . '/' . $endpoint . '/config/bootstrap.php';
+
+$config = yii\helpers\ArrayHelper::merge(
+	require __DIR__ . '/../' . $appfolder . '/common/config/main.php',
+	require __DIR__ . '/../' . $appfolder . '/common/config/main-local.php',
+	require __DIR__ . '/../' . $appfolder . '/' . $endpoint . '/config/main.php',
+	require __DIR__ . '/../' . $appfolder . '/' . $endpoint . '/config/main-local.php'
+);
+
+(new yii\web\Application($config))->run();
