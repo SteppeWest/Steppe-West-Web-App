41ea9d597ddbeb2fbef27eda71af3e7a3641dda3
p2made <pedrofp@me.com>
2024-10-16 00:09:07 +1000

    Gii SWCurrentLanguage

---
+++ app/letter/models/SWCurrentLanguageSearch.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace letter\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use letter\models\SWCurrentLanguage;
+
+/**
+ * SWCurrentLanguageSearch represents the model behind the search form of `letter\models\SWCurrentLanguage`.
+ */
+class SWCurrentLanguageSearch extends SWCurrentLanguage
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['pk'], 'integer'],
+            [['code', 'page', 'title', 'subtitle', 'description', 'keywords', 'lead', 'origin', 'origin_link', 'body_yaml'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = SWCurrentLanguage::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'pk' => $this->pk,
+        ]);
+
+        $query->andFilterWhere(['like', 'code', $this->code])
+            ->andFilterWhere(['like', 'page', $this->page])
+            ->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'subtitle', $this->subtitle])
+            ->andFilterWhere(['like', 'description', $this->description])
+            ->andFilterWhere(['like', 'keywords', $this->keywords])
+            ->andFilterWhere(['like', 'lead', $this->lead])
+            ->andFilterWhere(['like', 'origin', $this->origin])
+            ->andFilterWhere(['like', 'origin_link', $this->origin_link])
+            ->andFilterWhere(['like', 'body_yaml', $this->body_yaml]);
+
+        return $dataProvider;
+    }
+}
---
+++ app/letter/views/current/update.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+/** @var yii\web\View $this */
+/** @var letter\models\SWCurrentLanguage $model */
+
+$this->title = 'Update Sw Current Language: ' . $model->title;
+$this->params['breadcrumbs'][] = ['label' => 'Sw Current Languages', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'pk' => $model->pk]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="swcurrent-language-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
