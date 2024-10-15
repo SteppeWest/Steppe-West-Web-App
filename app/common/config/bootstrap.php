950249632a25f711f399524477a5a9d5d80d8a06
p2made <pedrofp@me.com>
2024-10-15 19:13:44 +1000

    remove unnecessary endpoint

--- app/common/config/bootstrap.php
+++ app/common/config/bootstrap.php
@@ -1,7 +1,10 @@
 <?php
-Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@common',   dirname(__DIR__));
+Yii::setAlias('@assets',   dirname(__DIR__) . '/assets');
+Yii::setAlias('@backend',  dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('@letter', dirname(dirname(__DIR__)) . '/letter');
-Yii::setAlias('@links', dirname(dirname(__DIR__)) . '/links');
+Yii::setAlias('@letter',   dirname(dirname(__DIR__)) . '/letter');
+Yii::setAlias('@links',    dirname(dirname(__DIR__)) . '/links');
+Yii::setAlias('@members',  dirname(dirname(__DIR__)) . '/members');
+Yii::setAlias('@cdn',      dirname(dirname(__DIR__)) . '/cdn');
+Yii::setAlias('@console',  dirname(dirname(__DIR__)) . '/console');
