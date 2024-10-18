596185b99453e23435aca64467f67f0e5e34417c
p2made <pedrofp@me.com>
2024-10-18 15:24:17 +1000

    Update SWCurrentLanguageController.php

--- app/language/controllers/SWCurrentLanguageController.php
+++ app/language/controllers/SWCurrentLanguageController.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * SWCurrentLanguageController.php
+ *
+ * @author Pedro Plowman
+ * @copyright Copyright (c) 2024 Steppe West
+ * @link https://steppewest.com/
+ * @license MIT
+ */
 
 namespace language\controllers;
 
@@ -76,7 +84,6 @@ class SWCurrentLanguageController extends Controller
 		} else {
 			$model->loadDefaultValues();
 		}
-
 		return $this->render('create', [
 			'model' => $model,
 		]);
@@ -96,7 +103,6 @@ class SWCurrentLanguageController extends Controller
 		if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
 			return $this->redirect(['view', 'page_pk' => $model->page_pk]);
 		}
-
 		return $this->render('update', [
 			'model' => $model,
 		]);
