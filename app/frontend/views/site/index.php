3485fcbce263bf03cbe7f184fef4cbe46685394d
p2made <pedrofp@me.com>
2024-10-22 15:19:07 +1000

    cleanup

--- app/frontend/views/site/index.php
+++ app/frontend/views/site/index.php
@@ -2,51 +2,79 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = 'Bridging Cultures: Steppe West‘s Invitation to Central Asia';
 ?>
-<div class="site-index">
-	<div class="p-5 mb-4 bg-transparent rounded-3">
-		<div class="container-fluid py-5 text-center">
-			<h1 class="display-4">Congratulations!</h1>
-			<p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
-			<p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
+<section class="py-1">
+	<?= $this->render('//partials/languages.min.php') ?>
+
+	<div class="container my-4">
+		<div class="align-items-center justify-content-center row">
+			<div class="col-lg-1 order-3 order-lg-1"></div>
+			<div class="col-lg-6 order-1 order-lg-2">
+				<p class="lead">Dear Friends in Central Asia,</p>
+				<p>Allow me to introduce myself as the curator behind Steppe West - a platform dedicated to bridging the gap between the rich cultures of Central Asia and English speaking audiences worldwide. My journey into the captivating world of Central Asian traditions and stories has ignited a desire to share these treasures with a global audience that often remains unaware of the wonders this region beholds.</p>
+			</div>
+			<div class="col-lg-3 order-2 order-lg-3">
+				<img class="img-fluid rounded-circle" alt="Central Asia" src="https://placehold.co/500">
+			</div>
+		</div>
+		<div class="align-items-center justify-content-center row">
+			<div class="col-lg-3 order-2 order-lg-1">
+				<img class="img-fluid rounded-circle" alt="Central Asia" src="https://placehold.co/500">
+			</div>
+			<div class="col-lg-6 order-1 order-lg-2">
+				<p>With Steppe West, I aim to serve as an English language publisher to showcase the music, arts, cuisines, histories, and daily lives of Central Asian peoples. It is my strong belief that all Turkic and Mongol cultures form an integral part of the vast cultural family that Steppe West seeks to celebrate and share with the world.</p>
+			</div>
+			<div class="col-lg-1 order-3 order-lg-3"></div>
+		</div>
+		<div class="align-items-center justify-content-center row">
+			<div class="col-lg-1 order-3 order-lg-1"></div>
+			<div class="col-lg-6 order-1 order-lg-2">
+				<p>My vision is not to simply extract content from you but to collaborate closely, ensuring that your stories are presented authentically and respectfully to an English speaking audience. Together, we can unravel the beauty and depth of Central Asia, fostering a greater understanding and appreciation among Western audiences.</p>
+			</div>
+			<div class="col-lg-3 order-2 order-lg-3">
+				<img class="img-fluid rounded-circle" alt="Central Asia" src="https://placehold.co/500">
+			</div>
+		</div>
+		<div class="align-items-center justify-content-center row">
+			<div class="col-lg-3 order-2 order-lg-1">
+				<img class="img-fluid rounded-circle" alt="Central Asia" src="https://placehold.co/500">
+			</div>
+			<div class="col-lg-6 order-1 order-lg-2">
+				<p>I am excited to share that I am currently working alongside a talented young man from Uzbekistan, enriching our team with his unique perspectives and insights. Our goal is to highlight the diverse tapestry of Central Asia through various platforms such as Facebook, Instagram, Twitter, YouTube, TikTok, and more. Each piece of content provided will be credited to its rightful creators, accompanied by links to further immerse our audience in your world.</p>
+			</div>
+			<div class="col-lg-1 order-3 order-lg-3"></div>
+		</div>
+		<div class="align-items-center justify-content-center row">
+			<div class="col-lg-1 order-3 order-lg-1"></div>
+			<div class="col-lg-6 order-1 order-lg-2">
+				<p>Your contributions can range from music and arts to cuisine, sports, history, nature, and beyond. Whether it‘s a heartfelt story, a captivating video, or a glimpse into your daily life, we welcome all forms of content that you believe would captivate and intrigue English speaking audiences. Our collaboration is not about taking from you but sharing your voices and narratives on a global stage.</p>
+			</div>
+			<div class="col-lg-3 order-2 order-lg-3">
+				<img class="img-fluid rounded-circle" alt="Central Asia" src="https://placehold.co/500">
+			</div>
+		</div>
+		<div class="align-items-center justify-content-center row">
+			<div class="col-lg-3 order-2 order-lg-1">
+				<img class="img-fluid rounded-circle" alt="Central Asia" src="https://placehold.co/500">
+			</div>
+			<div class="col-lg-6 order-1 order-lg-2">
+				<p>Should you be interested in participating, or have any questions, suggestions, or content to share, please do not hesitate to reach out. Your involvement in Steppe West could be the beginning of a powerful cultural exchange, enriching lives and inspiring connections across borders.</p>
+			</div>
+			<div class="col-lg-1 order-3 order-lg-3"></div>
+		</div>
+		<div class="align-items-center justify-content-center row">
+			<div class="col-lg-1 order-3 order-lg-1"></div>
+			<div class="col-lg-6 order-1 order-lg-2">
+				<p>Furthermore, if you find that Steppe West may not align with your current endeavours but know of individuals who might resonate with our mission, I kindly encourage you to share this letter with them. Together, we can expand our network of cultural enthusiasts and creators, fostering a community of mutual appreciation and collaboration.</p>
+				<p>Warm regards,</p>
+				<p>Pedro</p>
+			</div>
+			<div class="col-lg-3 order-2 order-lg-3">
+				<img class="img-fluid rounded-circle" alt="Central Asia" src="https://placehold.co/500">
+			</div>
 		</div>
 	</div>
 
-	<div class="body-content">
-
-		<div class="row">
-			<div class="col-lg-4">
-				<h2>Heading</h2>
-
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-					ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-					fugiat nulla pariatur.</p>
-
-				<p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-			</div>
-			<div class="col-lg-4">
-				<h2>Heading</h2>
-
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-					ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-					fugiat nulla pariatur.</p>
-
-				<p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-			</div>
-			<div class="col-lg-4">
-				<h2>Heading</h2>
-
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-					ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-					fugiat nulla pariatur.</p>
-
-				<p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-			</div>
-		</div>
-
-	</div>
-</div>
+	<?= $this->render('//partials/content.foot.min.php') ?>
+</section>
