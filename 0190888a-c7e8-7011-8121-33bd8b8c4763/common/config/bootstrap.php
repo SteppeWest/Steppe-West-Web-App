86bd1d0070c75f2dc496904286e7921c275d5dc4
p2made <pedrofp@me.com>
2024-12-26 01:19:54 +1000

    reset

--- 0190888a-c7e8-7011-8121-33bd8b8c4763/common/config/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('@letter', dirname(dirname(__DIR__)) . '/letter');
-Yii::setAlias('@links', dirname(dirname(__DIR__)) . '/links');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
