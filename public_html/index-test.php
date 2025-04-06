c8491e27a04a6153e56f7ecbce5687f3df80d018
p2made <pedrofp@me.com>
2025-04-07 03:14:35 +1000

    htaccess

--- public_html/index-test.php
+++ public_html/index-test.php
@@ -15,7 +15,6 @@ require __DIR__ . '/../' . $appfolder . '/vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../' . $appfolder . '/common/config/bootstrap.php';
 require __DIR__ . '/../' . $appfolder . '/' . $endpoint . '/config/bootstrap.php';
 
-
 $config = yii\helpers\ArrayHelper::merge(
 	require __DIR__ . '/../' . $appfolder . '/common/config/main.php',
 	require __DIR__ . '/../' . $appfolder . '/common/config/main-local.php',
@@ -28,3 +27,4 @@ $config = yii\helpers\ArrayHelper::merge(
 );
 
 (new yii\web\Application($config))->run();
+
