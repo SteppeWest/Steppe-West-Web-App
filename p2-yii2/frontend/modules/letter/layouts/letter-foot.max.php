9cbdcadc5eba465ccec922678e39b10fb9e99690
p2made <pedrofp@me.com>
2025-06-22 01:00:06 +1000

    zing

--- p2-yii2/frontend/modules/letter/layouts/letter-foot.max.php
+++ p2-yii2/frontend/modules/letter/layouts/letter-foot.max.php
@@ -32,7 +32,9 @@ use common\widgets\SwContactObfuscator;
 		</div>
 		<div class="col-lg-4 float-end">
 			<p class="float-end fs-2">
+
 				<?= $this->params['socialButtons'] ?>
+
 			</p>
 		</div>
 	</div>
