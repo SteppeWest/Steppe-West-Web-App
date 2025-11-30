e558f0f79bf70df284b49f425c3e58f68a8361d3
Pedro Plowman <pedrofp@me.com>
2025-12-01 08:27:07 +1000

    Update SiteController.php

--- p2-yii2/frontend/controllers/SiteController.php
+++ p2-yii2/frontend/controllers/SiteController.php
@@ -29,16 +29,16 @@ class SiteController extends Controller
 		return [
 			'access' => [
 				'class' => AccessControl::class,
-				'only' => ['logout', 'signup'],
+				'only' => ['login', 'logout', 'signup'],
 				'rules' => [
 					[
-						'actions' => ['signup'],
 						'allow' => true,
+						'actions' => ['login', 'signup'],
 						'roles' => ['?'],
 					],
 					[
-						'actions' => ['logout'],
 						'allow' => true,
+						'actions' => ['logout'],
 						'roles' => ['@'],
 					],
 				],
@@ -256,4 +256,14 @@ class SiteController extends Controller
 			'model' => $model
 		]);
 	}
+}
+
+?>
+<?php
+class SiteController extends Controller
+{
+	public function behaviors()
+	{
+	}
+	// ...
 }
