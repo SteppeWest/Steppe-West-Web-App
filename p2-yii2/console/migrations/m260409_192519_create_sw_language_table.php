48c03c42bd23fef66f3815dfb9a1a01e296e011d
Pedro Plowman <pedrofp@me.com>
2026-04-10 05:44:37 +1000

    migrations

---
+++ p2-yii2/console/migrations/m260409_192519_create_sw_language_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%sw_language}}`.
+ */
+class m260409_192519_create_sw_language_table extends Migration
+{
+	public function safeUp()
+	{
+		$this->createTable('{{%sw_language}}', [
+			'id' => $this->primaryKey(),
+			'code' => $this->string(8)->notNull(),
+			'legacy_code' => $this->string(8)->null(),
+			'menu_position' => $this->smallInteger()->null(),
+			'is_active' => $this->boolean()->notNull()->defaultValue(true),
+			'name' => $this->string(64)->notNull(),
+			'native_name' => $this->string(64)->notNull(),
+			'flag_icon' => $this->string(8)->null(),
+			'ui_label' => $this->string(16)->notNull(),
+			'locale' => $this->string(16)->notNull(),
+			'html_lang' => $this->string(16)->notNull(),
+		]);
+
+		$this->createIndex(
+			'ux_sw_language_code',
+			'{{%sw_language}}',
+			'code',
+			true
+		);
+
+		$this->createIndex(
+			'ux_sw_language_legacy_code',
+			'{{%sw_language}}',
+			'legacy_code',
+			true
+		);
+	}
+
+	public function safeDown()
+	{
+		$this->dropTable('{{%sw_language}}');
+	}
+}
