d497ede34c0f59d9d4761a076e33658ae623ae05
Pedro Plowman <pedrofp@me.com>
2026-04-10 22:52:55 +1000

    Update PageQuery.php

--- p2-yii2/common/models/PageQuery.php
+++ p2-yii2/common/models/PageQuery.php
@@ -9,4 +9,18 @@ namespace common\models;
  */
 class PageQuery extends \common\models\generated\PageQuery
 {
+	public function active(): static
+	{
+		return $this->andWhere(['is_active' => 1]);
+	}
+
+	public function home(): static
+	{
+		return $this->andWhere(['is_home' => 1]);
+	}
+
+	public function byCode(string $code): static
+	{
+		return $this->andWhere(['code' => $code]);
+	}
 }
